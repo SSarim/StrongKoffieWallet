0df72b6dbb3c5bb534ee426c13d8edbe7b51d8c0
SSarim <sarimshahwar@gmail.com>
2025-03-02 16:36:06 -0500

    setting and testing possibe login system

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,3 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-
-from app.auth import authenticate_user, fake_users_db
 from app.models import TransactionModel
 from app.blockchain import Transaction, Blockchain
 
@@ -23,7 +19,7 @@ router = APIRouter()
 blockchain = Blockchain()
 
 
-@router.post("/authToken")
+@router.post("/token")
 async def login_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db,form_data.username, form_data.password)
     if not user:
@@ -37,9 +33,17 @@ async def login_token(form_data: OAuth2PasswordRequestForm = Depends()):
         data={"sub" : user["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+# NEED TO FIX SO I CAN SEE IN LOCAL HOST DOCS
+
+
 
 @router.post("/transaction/")
-async def create_transaction(tx: TransactionModel):
+async def create_transaction(tx: TransactionModel, current_user: dict = Depends(get_current_user)):
+#checking if the sender is the current user which is logged in
+    if tx.sender != current_user["username"]:
+        raise HTTPException(status_code=403, detail = "Unauthorized, please login to your own personal account!")
+
+
     # Edge case: Sender fund check before adding the transaction
     sender_balance = blockchain.get_balance(tx.sender)
     if sender_balance < tx.amount:
@@ -49,14 +53,15 @@ async def create_transaction(tx: TransactionModel):
     return {"message": "Transaction added", "transaction": transaction.to_dict()}
 
 @router.post("/mine/")
-async def mine_transactions(miner_address: str):
+async def mine_transactions(miner_address: str, current_user: dict = Depends(get_current_user)):
     block = blockchain.mine_pending_transactions(miner_address)
+    # Testing: need to check in login required
     if block is None:
         raise HTTPException(status_code=400, detail="No transactions to mine")
     return {"message": "Block mined", "block": block.__dict__}
 
 @router.get("/balance/{address}")
-async def get_balance(address: str):
+async def get_balance(address: str, current_user: dict = Depends(get_current_user)):
     balance = blockchain.get_balance(address)
     return {"address": address, "balance": balance}
 
@@ -64,3 +69,5 @@ async def get_balance(address: str):
 async def get_transaction_history():
     history = blockchain.get_transaction_history()
     return {"transactions": history}
+# need to see if I should have transactions available for public viewing or only auth users who are logged in.
+# current_user : dict = Depends(get_current_user)
