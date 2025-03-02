000da596ceca678cf56cb9e5b0eac42e39c39d2c
SSarim <sarimshahwar@gmail.com>
2025-03-02 16:02:04 -0500

    setting and testing possibe login system

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,42 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.auth import authenticate_user, fake_users_db
 from app.models import TransactionModel
 from app.blockchain import Transaction, Blockchain
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from app.auth import (
+    authenticate_user,
+    create_access_token,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    oauth2_scheme,
+    get_current_user,
+    fake_users_db
+)
 
 router = APIRouter()
 
 # Global blockchain instance
 blockchain = Blockchain()
+
+
+@router.post("/authToken")
+async def login_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db,form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub" : user["username"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/transaction/")
 async def create_transaction(tx: TransactionModel):
