7e7dac95c6d7082237509273b649bf218cd47fff
SSarim <sarimshahwar@gmail.com>
2025-04-01 00:58:52 -0400

    Cleanup main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.routes import router as blockchain_router
 from app.database import engine, Base
 
+
+# creating DB if not built already
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="BobbyShmurda:TheKingOfNY")
-
 app.include_router(blockchain_router)
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
